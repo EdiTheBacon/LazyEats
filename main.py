691d240f3ad5a0a233a85e2f58847561515875ec
Eric Deng <eric.rrdeng@gmail.com>
2024-02-11 13:49:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+import openai
+from openai import OpenAI
+import os
+import random
+from special_foods import special_foods
+
+from flask import Flask, render_template, url_for, request, jsonify
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('page1.html', posts=posts)
+
+
+@app.route("/ourGoals")
+def goals():
+    return render_template('page2.html', title='OurGoals')
+
+
+@app.route("/About")
+def about():
+    return render_template('page3.html', title='OurGoals')
+
+
+@app.route("/services")
+def search_page():
+    return render_template("Functional.html", title="search page")
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    if query.lower() in special_foods.keys():
+        query = random.choice(special_foods[query.lower()])
+    # Perform the search logic based on the query
+
+    # Example: Return a JSON response
+    client = OpenAI(
+        api_key='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')
+    response = client.chat.completions.create(model="gpt-3.5-turbo",
+                                              messages=[{"role": "system",
+                                                         "content": "You are the world's best cook. You can tell what ingredients are present in any given meal just from hearing the dish's name. When given a dish's name, you will first list out  all of it's ingredient in subsequent order with no elaboration in the format: (Ingredients: a b c (comma separated, and all on the same with no additional detail in parentheses and no comma used elsewhere)...)\
+                                                             followed by a recipe to create that dish"},
+                                                        {"role": "user",
+                                                         "content": f"{query}"}], temperature=0)
+
+    response = response.choices[0].message.content.splitlines()
+    ingredients = response[0]
+    recipe = response[2:]
+    # ingredients = "Ingredients: flour, water, salt, filling of choice , soy sauce, sesame oil, ginger, garlic, green onions"
+    # recipe = []
+    ls = []
+    for i in range(len(ingredients)):
+        if ingredients[i] in ["(", ")"]:
+            ls.append(i)
+    while not (len(ls) % 2) and ls:
+        ingredients = ingredients[:ls[0]] + ingredients[ls[1]+1:]
+        ls.pop(0)
+        ls.pop(0)
+    ingredients = ingredients.split(",")[1:]
+    ingredients = ''.join(
+        [ingredient.strip()+"\n" for ingredient in ingredients])
+    ingredients = f"Dish: {query}\n{ingredients}"
+    print(ingredients)
+    recipe = ''.join([line + "\n" for line in recipe])
+
+    return jsonify([ingredients, recipe])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ special_foods.py
@@ -0,0 +1,61 @@
+special_foods = {"vegan": [
+    "Creamy Broccoli Vegan Pasta",
+    "Butternut Squash Stuffed Shells",
+    "Spaghetti Bolognese (Vegan Version)",
+    "Creamy Pasta Pomodoro",
+    "Best Vegan Pizza",
+    "Creamy Vegan Pasta Salad",
+    "Broccoli Pasta Salad",
+    "Creamy Shiitake & Kale Pasta"
+], "vegetarian":  [
+    "Extra Vegetable Fried Rice",
+    "Best Lentil Soup",
+    "Crispy Baked Falafel",
+    "Epic Vegetarian Tacos",
+    "Peanut Slaw with Soba Noodles",
+    "Walnut-Lentil Bolognese",
+    "Vegetable Soup",
+    "Roasted Sweet Potato Salad",
+    "Shaved Brussels Sprout Salad with Creamy Maple Dressing",
+    "Vegetarian Burrito Bowl with Avocado Crema"
+], "halal": [
+    "Turmeric Chicken on Flatbread",
+    "Stir-Fried Chicken and Vegetables",
+    "Sweet and Sour Shrimp",
+    "Salisbury Steak and Sauce",
+    "Classic Butter Chicken",
+    "Chicken Liver Adobo (Adobong Atay)",
+    "Chicken Shawarma Rice",
+    "Scrambled Eggs with Mushrooms",
+    "Authentic Chicken Maqluba",
+    "Saucy Halal Beef on Bread",
+    "Halal Quinoa Kibbeh Labaniyeh"
+], "kosher": [
+    "Turmeric Chicken on Flatbread",
+    "Stir-Fried Chicken and Vegetables",
+    "Sweet and Sour Shrimp",
+    "Salisbury Steak and Sauce",
+    "Classic Butter Chicken",
+    "Chicken Liver Adobo (Adobong Atay)",
+    "Chicken Shawarma Rice",
+    "Spaghetti Squash Sopa Seca (Paleo and Gluten-Free)",
+    "Chili with Butternut Squash",
+    "Chicken and Peppers",
+    "Ramen Shakshuka",
+    "Avocado Stuffed Salmon with Wild Rice",
+    "Mushroom Souffle",
+    "Roasted Fruit Salad",
+    "Fruit Salad with Strawberry Sauce",
+    "Squash and Pear Soup",
+    "Roasted String Beans with Apricots and Dates"
+], "allergies": [
+    "Turmeric Chicken on Flatbread",
+    "Stir-Fried Chicken and Vegetables",
+    "Sweet and Sour Shrimp",
+    "Salisbury Steak and Sauce",
+    "Classic Butter Chicken",
+    "Chicken Liver Adobo (Adobong Atay)",
+    "Chicken Shawarma Rice",
+    "Spaghetti Squash Sopa Seca (Paleo and Gluten-Free)",
+    "Chili with Buttern"]
+}
